9482909c17098e430f299bee9682423effafae23
Max <Maximillian.Brown.2021@student.lhschools.org>
2018-10-04 13:02:14 -0400

    Goofed had to stage all the files

--- src/Rock_Paper_Scissors.java
+++ src/Rock_Paper_Scissors.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Rock_Paper_Scissors {
+
+    public static void main(String[]args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+    }
 }

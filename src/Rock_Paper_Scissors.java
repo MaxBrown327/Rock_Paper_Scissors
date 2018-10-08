3e3e074c9e69c61e9cac5cd6baf5d9502ea15b87
Max <Maximillian.Brown.2021@student.lhschools.org>
2018-10-08 14:16:07 -0400

    Adding some more stuffs

--- src/Rock_Paper_Scissors.java
+++ src/Rock_Paper_Scissors.java
@@ -4,17 +4,37 @@ public class Rock_Paper_Scissors {
 
     public static void main(String[]args) {
 
+        int numberOfTrys;
+        int systemNumber;
+        int playerChoice;
         boolean playAgain;
+        boolean wonGame;
         Scanner keyboard = new Scanner(System.in);
-        
+
+        playAgain = false;
 
 
 
-
-
+        //The Actual Game\\
         while (playAgain==false) {
-            playAgain = false;
 
+            wonGame = false;
+            numberOfTrys = 5;
+
+
+            systemNumber = (int) (Math.random() * 100);
+            System.out.println("Welcome to a game of Rock paper scissors!");
+            System.out.println("Try and beat the game!");
+
+            while (!wonGame && numberOfTrys > 0) {
+                System.out.println("Pick Rock (1) Paper (2) or Scissors (3)");
+                playerChoice = keyboard.nextInt();
+
+                if (systemNumber >34 && playerChoice> 34) {
+
+                }
+
+            }
 
         }
 
